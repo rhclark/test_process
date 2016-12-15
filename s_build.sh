5d6ce734e046a504d16304b7965cf81279d7c2b5
Robert Clark <rhclark.pitt@gmail.com>
2016-12-14 19:52:38 -0500

    Another trivial change

--- s_build.sh
+++ s_build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-echo '<html><head><title> Deploy Test </title></script></head><body><center><h1>Deploy Test</h1>Deploy Test</center></body></html>' > index.html
+echo '<html><head><title>Deploy Test</title></script></head><body><center><h1>Deploy Test</h1>Deploy Test</center></body></html>' > ./index.html
